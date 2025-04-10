5a560ae8213e6e435c20ae6b63858442a5bbfe77
Luan Victor de Faria Brito <luanvct2003@gmail.com>
2025-04-10 13:07:30 -0300

    10/04

---
+++ Exercícios - Const e Let/index.js
@@ -0,0 +1,11 @@
+/*LUIZ OTAVIO MIRANDA tem 30 anos, pesa 84 kg tem 1.8 de altura e seu IMC é de 25.925925925925925
+Luiz Otáviio nasceu em 1995
+*/
+const nome='Luiz Otávio';
+const sobrenome='Miranda';
+const idade=30;
+const peso=84;
+const altura=1.80;
+let imc = peso/(altura*altura);
+let anoNascimento = 2025-30;
+console.log(`${nome} ${sobrenome} tem ${idade} anos, pesa ${peso} e tem ${altura} e seu IMC é de ${imc}, ele nasceu em ${anoNascimento}`);
---
+++ aula2/index.js
@@ -0,0 +1,3 @@
+console.log('Olá mundo');
+console.log('É isso!');
+window.alert('Alooooo!') 
