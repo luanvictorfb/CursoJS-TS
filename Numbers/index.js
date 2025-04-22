8edd592ea8ab829ccb989df0e8fa813115adfcb6
Luan Victor de Faria Brito <luanvct2003@gmail.com>
2025-04-22 11:40:05 -0300

    Update index.js

--- Numbers/index.js
+++ Numbers/index.js
@@ -1,7 +1,31 @@
-let num1 = 10.57819384701394;
-let num2 = 2.5;
+// IEEE 754-2008
+let num1 = 0.7;
+let num2 = 0.1;
+
+num1 += num2; //0.8
+num1 += num2; //0.9
+num1 += num2; //1.0
+num1 += num2; //1.1
+num1 += num2; //1.2
+num1 += num2; //1.3
+num1 += num2; //1.4
+num1 += num2; //1.5
+num1 += num2; //1.6
+num1 += num2; //1.7
+num1 += num2; //1.8
+num1 += num2; //1.9
+num1 += num2; //2.0
+ 
+num1 = Number(num1.toFixed(2));
+
+console.log(num1)
+console.log(Number.isInteger(num1));
 
 // console.log(num1.toString()+num2);
 // num1 = num1.toString();
 // console.log(num1.toString(2));
-console.log(num1.toFixed(2));
+// console.log(num1.toFixed(4));
+// console.log(Number.isInteger(num1));
+// let temp = num1 * '5';
+// console.log(Number.isNaN(temp));
+// console.log(temp)
