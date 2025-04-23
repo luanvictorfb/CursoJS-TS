a4add71a1f05be8e52a97806d6af338632a0044e
Luan Victor de Faria Brito <luanvct2003@gmail.com>
2025-04-23 09:18:20 -0300

    Create index.js

---
+++ 27/index.js
@@ -0,0 +1,10 @@
+let num1 = 9.49;
+// let num2 = Math.floor(num1);
+// let num2 = Math.ceil(num1);
+// let num2 = Math.round(num1);
+// console.log(Math.max(1,2,3,4,5,6,7,-10,-100,-50,1500,8,9));
+// console.log(Math.min(1,2,3,4,5,6,7,-10,-100,-50,1500,8,9));
+// const aleatorio = Math.round(Math.random() * (10-5) + 5);
+// console.log(aleatorio);
+console.log(Math.pow(2,2)); 
+console.log(2**2); 
