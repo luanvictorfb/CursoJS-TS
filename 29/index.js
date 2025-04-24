63ca18abed5568546883b406d70e434fe9107a9f
Luan Victor de Faria Brito <luanvct2003@gmail.com>
2025-04-24 09:26:32 -0300

    23/04

---
+++ 29/index.js
@@ -0,0 +1,36 @@
+const alunos = ['Luiz','Maria','João'];
+
+console.log(typeof alunos);
+console.log(alunos instanceof Array);
+
+
+
+
+// alunos.push('Luan');
+// alunos.push('Coradi');
+
+// delete alunos[1];
+
+// POSSIVEL COLOCAR UM POP OU SHIFT EM UMA VARIÁVEL 
+// const temp = alunos.shift();
+// const temp = alunos.pop();
+
+// alunos.shift(); REMOVE DO INICIO
+
+// alunos.pop(); REMOVE DO FINAL
+
+// alunos.unshift('Luan');
+// alunos.unshift('Coradi'); ADICIONAR NO COMEÇO
+
+// alunos.push('Luan');
+// alunos[alunos.length]='Luan';
+// alunos[alunos.length]='Coradi'; ADICIONA NO FIM
+
+// alunos[0] = 'Luan';
+// alunos[3] = 'Coradi'; ADICIONA OU ALTERA
+
+// const nome = 'Luan;
+// console.log(nome[0]);
+// console.log(alunos[0]);
+// console.log(alunos[1]);
+// console.log(alunos[2]);
---
+++ 30/index.js
@@ -0,0 +1,17 @@
+function soma(x,y){
+    return x+y;
+}
+console.log(soma(1,4));
+console.log(soma(2,2));
+console.log(soma(10,10));
+
+
+
+
+
+
+/*function saudacao(nome){
+    return `Bom Dia ${nome}!`;
+}
+const variavel = saudacao('Luan');
+console.log(variavel);*/
