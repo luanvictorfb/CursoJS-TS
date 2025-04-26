440e182a0eebe742cdb4f7ddeff622d411c5d0a2
Luan Victor de Faria Brito <luanvct2003@gmail.com>
2025-04-26 15:31:43 -0300

    26/04

---
+++ Seção 3/40/index.js
@@ -0,0 +1,12 @@
+// const hora = 17;
+
+// if(hora<12 && hora > 0){
+//     console.log('Bom dia!');
+// } else if (hora <= 17 && hora>=12){
+//     console.log('Boa tarde!');
+// } else {
+//     console.log('Boa noite!');
+// }
+
+
+
