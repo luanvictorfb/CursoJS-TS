7088576cb20aeab4fc387b3aeb49e47502d68983
Luan Victor de Faria Brito <luanvct2003@gmail.com>
2025-05-02 19:03:48 -0300

    02/05²

---
+++ Seção 3/67 - lista/assests/js/main.js
@@ -0,0 +1,77 @@
+const inputTarefa = document.querySelector('.inputTarefa');
+const btnTarefa = document.querySelector('button');
+const tarefas = document.querySelector('.tarefas');
+
+
+
+btnTarefa.addEventListener('click', function () {
+    if (!inputTarefa.value) return;
+    criaTarefa(inputTarefa.value);
+});
+
+inputTarefa.addEventListener('keypress', function (e) {
+    if (e.keyCode === 13) {
+        if (!inputTarefa.value) return;
+        criaTarefa(inputTarefa.value);
+    }
+});
+
+document.addEventListener('click' , function(e){
+    const el = e.target;
+    if(el.classList.contains('apagar')){
+        el.parentElement.remove();
+        salvarTarefa();
+    }
+});
+
+
+function limpaInput(){
+    inputTarefa.value = '';
+    inputTarefa.focus();
+}
+
+function criaLi() {
+    const li = document.createElement('li');
+    return li;
+}
+
+function criaTarefa(textoInput) {
+    const li = criaLi();
+    li.innerHTML = textoInput;
+    tarefas.appendChild(li);
+    criaBotaoApagar(li);
+    limpaInput();
+    salvarTarefa();
+}
+
+function criaBotaoApagar(li){
+    li.innerHTML += '  '
+    const btnApagar = document.createElement('button');
+    btnApagar.innerText = 'Apagar';
+    btnApagar.setAttribute('class', 'apagar');
+    li.appendChild(btnApagar);
+}
+
+function salvarTarefa(){
+    const liTarefas = tarefas.querySelectorAll('li');
+    const listadeTarefas = [];
+
+    for (let tarefa of liTarefas    ){
+        let tarefaTexto = tarefa.innerText;
+        tarefaTexto = tarefaTexto.replace('Apagar' , '').trim();
+        listadeTarefas.push(tarefaTexto);
+    }
+
+    const tarefasJSON = JSON.stringify(listadeTarefas);
+    localStorage.setItem('tarefas', tarefasJSON);
+}
+
+function adicionaTarefasSalvas(){
+    const tarefas = localStorage.getItem('tarefas');
+    const listaDeTarefas = JSON.parse(tarefas);
+
+    for(let tarefa of listaDeTarefas){
+        criaTarefa(tarefa);
+    }
+}
+adicionaTarefasSalvas();
