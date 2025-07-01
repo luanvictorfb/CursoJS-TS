e4992ba8a105919d4ca1758ffe6eefede6cffa9b
Luan Victor de Faria Brito <luanvct2003@gmail.com>
2025-07-01 13:44:00 -0300

    Create 91.js

---
+++ Seção 4/91.js
@@ -0,0 +1,27 @@
+function Produto(nome,preco,estoque){
+    this.nome = nome;
+    this.preco = preco;
+    let estoquePriv =  estoque;
+
+    Object.defineProperty(this , 'estoque' , {
+        enumerable: true, // Mostra a chave quando true
+        configurable: false, // configurável ou reconfigurável quando true
+
+        get: function(){
+            return estoquePriv;
+        },
+        set: function(valor){
+            if (typeof valor !== Number){
+                console.log('Valor irreal')
+                return;
+            }
+            estoquePriv = valor;
+        }
+    });
+
+}
+
+const p1 = new Produto('Camiseta', 19.90 , 50);
+// console.log(p1);
+p1.estoque = 'O valor que eu quero';
+console.log(p1);
