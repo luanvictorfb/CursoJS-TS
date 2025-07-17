9a1d04f7de8033e80cc87dc73d569a6d0f4175e8
Luan Victor de Faria Brito <luanvct2003@gmail.com>
2025-07-17 08:58:13 -0300

    Create 105.js

---
+++ Seção 4/105.js
@@ -0,0 +1,26 @@
+class ControleRemoto{
+    constructor(tvModelo){
+        this.tvModelo = tvModelo;
+        this.volume = 0;
+    }
+
+    aumentarVolume(){
+        this.volume += 2;
+    }
+
+    diminuirVolume(){
+        this.volume -= 2;
+    }
+
+
+    static trocaPilha(){
+        console.log('ok, trocando pilhas.')
+    }
+}
+
+const controle1 = new ControleRemoto('LG');
+controle1.aumentarVolume();
+controle1.aumentarVolume();
+controle1.aumentarVolume();
+ControleRemoto.trocaPilha();
+console.log(controle1);
