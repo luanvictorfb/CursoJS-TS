62036eefe99fe1751d74d0b0aee66f421935bcc3
Luan Victor de Faria Brito <luanvct2003@gmail.com>
2025-08-04 12:18:27 -0300

    04/08

---
+++ Seção 9/118/public/assets/js/bundle.js
@@ -0,0 +1,25 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ (() => {
+
+throw new Error("Module build failed (from ./node_modules/babel-loader/lib/index.js):\nSyntaxError: E:\\Projetos\\CursoJS-TS\\Seção 9\\116\\src\\index.js: A class name is required. (1:6)\n\n\u001b[0m\u001b[31m\u001b[1m>\u001b[22m\u001b[39m\u001b[90m 1 |\u001b[39m \u001b[36mclass\u001b[39m \n \u001b[90m   |\u001b[39m       \u001b[31m\u001b[1m^\u001b[22m\u001b[39m\u001b[0m\n    at constructor (E:\\Projetos\\CursoJS-TS\\Seção 9\\116\\node_modules\\@babel\\parser\\lib\\index.js:367:19)\n    at Parser.raise (E:\\Projetos\\CursoJS-TS\\Seção 9\\116\\node_modules\\@babel\\parser\\lib\\index.js:6627:19)\n    at Parser.parseClassId (E:\\Projetos\\CursoJS-TS\\Seção 9\\116\\node_modules\\@babel\\parser\\lib\\index.js:13773:20)\n    at Parser.parseClass (E:\\Projetos\\CursoJS-TS\\Seção 9\\116\\node_modules\\@babel\\parser\\lib\\index.js:13472:10)\n    at Parser.parseStatementContent (E:\\Projetos\\CursoJS-TS\\Seção 9\\116\\node_modules\\@babel\\parser\\lib\\index.js:12794:21)\n    at Parser.parseStatementLike (E:\\Projetos\\CursoJS-TS\\Seção 9\\116\\node_modules\\@babel\\parser\\lib\\index.js:12767:17)\n    at Parser.parseModuleItem (E:\\Projetos\\CursoJS-TS\\Seção 9\\116\\node_modules\\@babel\\parser\\lib\\index.js:12744:17)\n    at Parser.parseBlockOrModuleBlockBody (E:\\Projetos\\CursoJS-TS\\Seção 9\\116\\node_modules\\@babel\\parser\\lib\\index.js:13316:36)\n    at Parser.parseBlockBody (E:\\Projetos\\CursoJS-TS\\Seção 9\\116\\node_modules\\@babel\\parser\\lib\\index.js:13309:10)\n    at Parser.parseProgram (E:\\Projetos\\CursoJS-TS\\Seção 9\\116\\node_modules\\@babel\\parser\\lib\\index.js:12625:10)\n    at Parser.parseTopLevel (E:\\Projetos\\CursoJS-TS\\Seção 9\\116\\node_modules\\@babel\\parser\\lib\\index.js:12615:25)\n    at Parser.parse (E:\\Projetos\\CursoJS-TS\\Seção 9\\116\\node_modules\\@babel\\parser\\lib\\index.js:14492:10)\n    at parse (E:\\Projetos\\CursoJS-TS\\Seção 9\\116\\node_modules\\@babel\\parser\\lib\\index.js:14526:38)\n    at parser (E:\\Projetos\\CursoJS-TS\\Seção 9\\116\\node_modules\\@babel\\core\\lib\\parser\\index.js:41:34)\n    at parser.next (<anonymous>)\n    at normalizeFile (E:\\Projetos\\CursoJS-TS\\Seção 9\\116\\node_modules\\@babel\\core\\lib\\transformation\\normalize-file.js:64:37)\n    at normalizeFile.next (<anonymous>)\n    at run (E:\\Projetos\\CursoJS-TS\\Seção 9\\116\\node_modules\\@babel\\core\\lib\\transformation\\index.js:22:50)\n    at run.next (<anonymous>)\n    at transform (E:\\Projetos\\CursoJS-TS\\Seção 9\\116\\node_modules\\@babel\\core\\lib\\transform.js:22:33)\n    at transform.next (<anonymous>)\n    at step (E:\\Projetos\\CursoJS-TS\\Seção 9\\116\\node_modules\\gensync\\index.js:261:32)\n    at E:\\Projetos\\CursoJS-TS\\Seção 9\\116\\node_modules\\gensync\\index.js:273:13\n    at async.call.result.err.err (E:\\Projetos\\CursoJS-TS\\Seção 9\\116\\node_modules\\gensync\\index.js:223:11)");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/index.js"]();
+/******/ 	
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
