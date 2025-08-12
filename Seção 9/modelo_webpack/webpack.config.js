ec802120d2beca198e681aa8906933488c6d044d
Luan Victor <luanvct2003@gmail.com>
2025-08-12 09:41:00 -0300

    12/08

--- Seção 9/modelo_webpack/webpack.config.js
+++ Seção 9/modelo_webpack/webpack.config.js
@@ -17,8 +17,10 @@ module.exports = {
                     presets: ['@babel/env']
                 }
             }
+        },{
+            test: /\.css$/,
+            use:['style-loader', 'css-loader']
         }]
-
     },
     devtool: 'source-map'
 };
